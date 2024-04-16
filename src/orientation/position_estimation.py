55751ded6d88396603a458974f4d05b5887ce72d
nnsaravanan <wowniteesh@gmail.com>
2024-04-16 18:32:07 -0400

    Update position_estimation.py

--- src/orientation/position_estimation.py
+++ src/orientation/position_estimation.py
@@ -5,14 +5,15 @@ class PositionEstimation:
         self.L = L  # length of camera mm
         self.d = 0.04 # diameter of ball
 
-    def calculate(self, x,y, cx, cy, d_pix):
+    def calculate(self, w, l, x, y, cx, cy, d_pix):
         """
+        (w,l) width and and lenght of the image
         (x,y) x and y coordinates of the center of the ping pong ball
         (cx,cy) center of the image 
         d_pix pixels of diameter of ping pong ball
         """
-        fx = self.F * self.W / x
-        fy = self.F * self.L / y
+        fx = self.F * self.W / w
+        fy = self.F * self.L / l
         zreal = (fx * self.d) / d_pix
         xreal = ((x-cx) * zreal) / fx
         yreal = ((y-cy) * zreal) / fy
